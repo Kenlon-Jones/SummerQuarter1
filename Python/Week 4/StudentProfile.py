29d43759e3c1f1a1008f3469fa1c074a1882a334
Kenlon-Jones <kenlon.jones@hgs.hiddengeniusproject.org>
2025-07-08 15:34:53 -0700

    Student Profile

---
+++ Python/Week 4/StudentProfile.py
@@ -0,0 +1,22 @@
+student = {}
+
+studentName = input("Enter your student's name: ")
+student["Name"] = studentName
+
+studentAge = input("Enter your student's age: ")
+student["Age"] = studentAge
+
+studentGrade = input("Enter your student's grade: ")
+student["Grade"] = studentGrade
+
+hobbies = []
+hobby = input("Enter your student's hobbies; Type 'done' when done: ").lower()
+hobbies.append(hobby)
+
+while hobby != "done":
+    hobby = input("Enter your student's hobbies; Type 'done' when done: ").lower()
+    hobbies.append(hobby)
+    
+student["Hobbies"] = hobbies
+
+print(student)
