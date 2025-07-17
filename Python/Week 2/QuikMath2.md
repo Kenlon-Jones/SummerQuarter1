e97f1b9b6eeb4c69e4033410096b776182547faf
Kenlon-Jones <kenlon.jones@hgs.hiddengeniusproject.org>
2025-07-17 13:27:46 -0700

    QuikMath2

---
+++ Python/Week 2/QuikMath2.md
@@ -0,0 +1,11 @@
+1. Define what a function is in your own words
+A function is block of code that does it's job.
+
+2. What key word is used to define a function?
+The key word is def.
+
+3. What does a return statement do?
+It retuens the funtion's results once it's completed.
+
+4. What is the difference between arguments and parameters?
+Parameters are the named placeholders in the function definition. Arguments are the real values you pass in when you call the function.
---
+++ Python/Week 2/QuikMath2.py
@@ -0,0 +1,24 @@
+#defing our add function
+def Add(addend1, addend2):
+    return addend1 + addend2
+#Call our Add function and store its output inside the sum variable
+sum = Add(5, 35)
+#print the sum varible
+print(sum)
+def sub(subend1, subend2):
+    return subend1 - subend2
+sum = sub(100, 40)
+print(sum)
+def Div(diviide1, diviide2):
+    return diviide1/diviide2
+sum = Div(2, 4)
+print(sum)
+def mul(multiply1, multiply2):
+    return multiply1 * multiply2
+sum = mul(14, 14)
+print(sum)
+
+
+
+
+
